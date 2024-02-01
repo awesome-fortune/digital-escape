dab9a0f1af1681e1ad4876fe2bf643f4745459c0
Fortune Maseko <fortune.maseko@johnlewis.co.uk>
2024-02-01 11:13:59 +0200

    minor content update

--- process.php
+++ process.php
@@ -1,20 +1,18 @@
 <?php
-
     $to = "info@digitalescape.co.za";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
-	$subject = $_REQUEST['subject'];
+	$number = $_REQUEST['number'];
     $headers = "From: $from";
     
 
     $fields = array();
     $fields{"name"} = "Name";
     $fields{"email"} = "E-mail";
-    $fields{"subject"} = "Subject";
+    $fields{"number"} = "Number";
     $fields{"message"} = "Message";
 
     $body = "Here is the message from www.digitalescape.co.za:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
 
-    $send = mail($to, $subject, $body, $headers);
-
+    $send = mail($to, "New mail from website :: $name" , $body, $headers);
 ?>
